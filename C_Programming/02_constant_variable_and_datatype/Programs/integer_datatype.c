834e842af1f51b7b3a62585a3059d0eca5dfabac
jakeshbohaju <j.xeniumsahaj@hotmail.com>
2023-09-25 00:13:03 +0545

    update dat type

---
+++ C_Programming/02_constant_variable_and_datatype/Programs/integer_datatype.c
@@ -0,0 +1,60 @@
+/*
+Author : Jakesh Bohaju
+
+Integer Data type:
+Six type of integer
+1) Integer => %d
+2) Unsigned Integer => %u
+3) Short Integers => %hd
+4) Unsigned Long Integers => %hu
+4) Long Integers => %ld
+4) Unsigned Long Integers => %lu
+
+Maximum value for int: 2147483647
+Maximum value for unsigned int: 4294967295
+Maximum value for short int: 32767
+Maximum value for unsigned short int: 65535
+Maximum value for long int: 2147483647
+Maximum value for unsigned long int: 4294967295
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void main()
+{
+    int integer_no;
+    short int short_integer;
+    long int long_integer;
+    unsigned int positive_integre;
+    unsigned short int positive_short_integer;
+    unsigned long int positive_long_integer;
+
+    printf("Maximum value for int: %d\n", INT_MAX);
+    printf("Maximum value for unsigned int: %u\n", UINT_MAX);
+    printf("Maximum value for short int: %d\n", SHRT_MAX);
+    printf("Maximum value for unsigned short int: %u\n", USHRT_MAX);
+    printf("Maximum value for long int: %ld\n", LONG_MAX);
+    printf("Maximum value for unsigned long int: %lu\n", ULONG_MAX);
+
+    printf("\nYou can input max values for variable as shown above.\n");
+
+    printf("Enter integer number : ");
+    scanf("%d", &integer_no);
+    printf("Enter unsigned integer number : ");
+    scanf("%u", &positive_integre);
+    printf("Enter short integer number : ");
+    scanf("%hd", &short_integer);
+    printf("Enter unsigned short integer number : ");
+    scanf("%hu", &positive_short_integer);
+    printf("Enter long integer number : ");
+    scanf("%ld", &long_integer);
+    printf("Enter unsigned long integer number : ");
+    scanf("%lu", &positive_long_integer);
+
+    printf("\nEntered Values\nInteger : %d\nUnsigned Integer : %u\nShort Integer : %hd\nUnsigned Short Integer : %hu\nLong Integer : %ld\nUnsigned Long Integer : %lu\n", integer_no, positive_integre, short_integer, positive_short_integer, long_integer, positive_long_integer);
+
+    system("pause");
+}
---
+++ C_Programming/02_constant_variable_and_datatype/Programs/swap_number.c
@@ -0,0 +1,48 @@
+// WAP to swap two number with and without using third variable
+
+/*
+Swap using third variable, example
+a = 2
+b = 5
+then our logic is,
+c = b i.e c = 5
+b = a i.e b = 2
+a = c i.e a = 5
+
+Swap weithout using third variable, example
+a = 5
+b = 2
+then our logic is,
+a = a + b  i.e a = 5+2 = 7
+b = a - b  i.e b = 7-2 = 5
+a = a - b  i.e a = 7-5 = 2
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void main() 
+{
+    int no1, no2, temp;
+    printf("Enter two number for swapping.\n");
+    scanf("%d%d", &no1, &no2);
+    
+    // swapping two number using third variable
+    printf("Swaping with using third variable 'temp'.\n");
+    printf("Before swap:\n\tno1 = %d\n\tno2 = %d\n ", no1, no2);
+    temp = no2;
+    no2 = no1;
+    no1 = temp;
+    printf("\nAfter Swap:\n\tno1 = %d\n\tno2 = %d\n", no1, no2);
+
+    // swapping two number without using third variable
+    printf("Swaping without using third variable 'temp'.\n");
+    printf("Updated values Before swap:\n\tno1 = %d\n\tno2 = %d\n ", no1, no2);
+    no1 = no1 + no2;
+    no2 = no1 - no2;
+    no1 = no1 - no2;
+    printf("\nAfter Swap:\n\tno1 = %d\n\tno2 = %d\n ", no1, no2);
+
+    system("pause");
+}
