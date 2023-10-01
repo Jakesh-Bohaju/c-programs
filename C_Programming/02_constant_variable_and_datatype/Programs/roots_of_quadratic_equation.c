df47e0edd3a12fbbf475d072cb4224128c55251c
jakeshbohaju <jakesh.bohaju@gmail.com>
2023-10-01 14:56:28 +0545

    update 02

--- C_Programming/02_constant_variable_and_datatype/Programs/roots_of_quadratic_equation.c
+++ C_Programming/02_constant_variable_and_datatype/Programs/roots_of_quadratic_equation.c
@@ -23,9 +23,24 @@ void main()
     scanf("%f", &b);
     printf("Enter constant : ");
     scanf("%f", &c);
-    root1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-    root2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-    printf("Root1 = %f\nRoot2 = %f\n", root1, root2);
+    if (a > 0)
+    {
+        float deter = sqrt(b * b - 4 * a * c);
+        if (deter >= 0)
+        {
+            root1 = (-b + deter) / (2 * a);
+            root2 = (-b - deter) / (2 * a);
+            printf("Root1 = %f\nRoot2 = %f\n", root1, root2);
+        }
+        else
+        {
+            printf("Roots are complex number.\n");
+        }
+    }
+    else
+    {
+        printf("Equation must be quadratic equation with non zero coefficient of x^2.\n");
+    }
 
     system("pause");
 }
